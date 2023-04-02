da72ded741848a23cbc7cd9efe5bb96cb439602e
ZlP3R <Z1pergx@gmail.com>
2023-04-02 15:34:08 +0200

    homework7

--- homework7/task1.py
+++ homework7/task1.py
@@ -1,29 +1,75 @@
-phone_book = {"name": "Serhii", "last name": "Polischuk", "phone number": "+380634683778"}
-user_comands = input("enter the command: ")
-if user_comands == "stats":
-    print(len(phone_book))
-if user_comands == "add":
-    add_name = input("enter name: ")
-    add_second_name = input("enter last name: ")
-    add_phone_number = input("enter phone number: ")
-    phone_book.update({"name of second user": add_name, "last name of second user": add_second_name, "phone number of second user": add_phone_number})
-    print(phone_book)
-if user_comands == "del":
-    delete_comands = input("enter what you want to delete: ")
-    if delete_comands == "delete name":
-        del phone_book["name"]
-    if delete_comands == "delete last name":
-        del phone_book["last name"]
-    if delete_comands == "delete phone number":
-        del phone_book["phone number"]
-    if delete_comands == "delete name of second user":
-        del phone_book["name of second user"]
-    if delete_comands == "delete last name of second user":
-        del phone_book["last name of second user"]
-    if delete_comands == "delete phone number of second user":
-        del phone_book["phone number of second user"]
-    print(phone_book)
-if user_comands == "show name":
-    print(phone_book.get("name"))
-    print(phone_book.get("last name"))
-    print(phone_book.get("phone number"))
+phone_book = [{"name": "Serhii", "phone number": "+380634683778"},
+              {"name": "Kiril", "phone number": "+380634532457"},
+              {"name": "sofia", "phone number": "+380506754823"},
+              {"name": "oleksandr", "phone number": "+380508945678"}]
+
+def comand_stats():
+        a = len(phone_book)
+        print(f"in phone book {a} contacts")
+def add_name():
+        lst = {}
+        lst["name"] = a
+        lst["phone number"] = g
+        b = input(f"enter data is correct? {lst}: ")
+        if b == "yes":
+            phone_book.append(lst)
+            print(phone_book)
+        if b == "no":
+            lst["name"] = input("enter user name: ")
+            lst["phone number"] = input("enter phone number: ")
+            phone_book.append(lst)
+            print(phone_book)
+def show_contact(name):
+    c = ''
+    for i in phone_book:
+        if name == i.get("name"):
+            c = name
+            print(f"Name: {i.get('name')}\nPhone number: {i.get('phone number')}")
+
+def delete_contact(name):
+    c = ''
+    for i in phone_book:
+        if name == i.get("name"):
+            c = name
+            phone_book.remove(i)
+
+def all_names_in_book():
+    for i in phone_book:
+        i.get("name")
+        print(i.get("name"))
+
+
+while True:
+    comands = input("enter comand: ")
+
+    if comands == "add":
+        a = input("enter user name: ")
+        g = input("enter phone number: ")
+        print(add_name())
+        exit()
+
+    if comands == "show contact":
+        d = input("enter name of contact: ")
+        print(show_contact(d))
+        if d == "close":
+            exit()
+
+    if comands == "stats":
+        print(comand_stats())
+        exit()
+
+    if comands == "delete":
+        e = input("enter name of contact: ")
+        print(delete_contact(e))
+        print (phone_book)
+        exit()
+
+    if comands == "list":
+        print(all_names_in_book())
+        exit()
+
+    if comands == "close":
+        exit()
+
+
+
