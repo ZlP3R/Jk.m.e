dab2a04e9751f1b5bfb34e13698a374f0131fd5e
ZlP3R <Z1pergx@gmail.com>
2023-05-04 23:30:13 +0200

    homework16

--- homework12/task1.2.py
+++ homework12/task1.2.py
@@ -1,12 +1,11 @@
 import time
 def my_func(func):
     def my_function():
-        print("function start time, and function name:")
-        func()
+        print(f"function start time: {time.ctime()} and function name:")
         print(func.__name__)
-        print("decorator end")
+        func()
     return my_function()
 
 @my_func
 def My_func1():
-    print(time.ctime())
+    print("decorator end")
